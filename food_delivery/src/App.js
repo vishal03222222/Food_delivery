4963998cb851b3a1996be7a59a754200f6304689
vishal <mgvishalctr@gmail.com>
2025-03-18 13:04:11 +0530

    commit

--- food_delivery/src/App.js
+++ food_delivery/src/App.js
@@ -1,12 +1,23 @@
 //import logo from './logo.svg';
 import './App.css';
 import Home from './Screens/Home';
-
+import {
+  BrowserRouter as Router,
+  Routes,Route
+} from "react-router-dom"
+import Login from './Screens/Login';
 function App() {
   return (
-    <>
-    <Home></Home>
-    </>
+    <Router>
+    <div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        
+      </Routes>
+    </div>
+  </Router>
+  
   );
 }
 
--- food_delivery/src/Screens/Home.js
+++ food_delivery/src/Screens/Home.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Footer from './Footer'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
-        <div><Navbar/></div>
-      <div>Body</div>
-      <div><Footer/></div>
-
+    <div><Navbar/></div>
+    <div><div className="card mt-3" style={{ "width": "18rem","maxHeight":"360px" }}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is important text
+            </p>
+            <div>
+    <p className="text">This is some important text.</p>
+    <div className="container w-100">
+        <select className="m-2 h-100 w-100 bg-success">
+            {Array.from(Array(6), (e, i) => {
+                return (
+                    <option key={i+1} value={i+1}> {i+1} </option>
+                );
+            })}
+        </select>
     </div>
+</div>
+
+        </div>
+    </div></div>
+    <div><Footer/></div>
+</div>
+
   )
 }
 
